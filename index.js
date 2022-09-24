275c30fdf0bb24f2d306086a73dc15122dda86c0
marinanc <marinancarrizo@gmail.com>
2022-09-24 05:55:06 -0300

    feat: fetch geolocation data
    Update index.js to fetch geolocation data from API
    
    API source: https://openweathermap.org/

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+const API_KEY = '9846de5a91553cc8b14a019fa3241ffa';
 
-const log = position => {
+const fetchData = position => {
+    const { latitude, longitude } = position.coords;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
+        .then(response => response.json())
+        .then(data => console.log(data))
     console.log(position);
 }
 const onload = () => {
-    navigator.geolocation.getCurrentPosition(log);
+    navigator.geolocation.getCurrentPosition(fetchData);
 }
