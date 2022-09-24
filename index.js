50856bb7e0930f7a4a31bce94f0a77c0fbd0f9f0
marinanc <marinancarrizo@gmail.com>
2022-09-24 15:19:02 -0300

    feat: set data on index.html
    Update .js and .html files to set data in index.html
    Data: location, description, humidity, pressure, temperature

--- index.js
+++ index.js
@@ -4,9 +4,27 @@ const fetchData = position => {
     const { latitude, longitude } = position.coords;
     fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
         .then(response => response.json())
-        .then(data => console.log(data))
-    console.log(position);
+        .then(data => setWeatherData(data));
 }
+
+const setWeatherData = data => {
+    console.log(data);
+    const weatherData = {
+        location: data.name,
+        description: data.weather[0].main,
+        humidity: data.main.humidity,
+        pressure: data.main.pressure,
+        temperature: data.main.temp,
+        date: 'date',
+    }
+
+    //Itera weatherData y devuelve las key, es decir: location, description, etc...
+    Object.keys(weatherData).forEach( key => {
+        //Settear info en index.html
+        document.getElementById(key).textContent = weatherData[key];
+    });
+}
+
 const onload = () => {
     navigator.geolocation.getCurrentPosition(fetchData);
 }
